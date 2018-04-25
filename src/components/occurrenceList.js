4eeb2227ab507a935cde4e6dcc5607fa5f03b576
Igor Phelype Guimarães <igor.phelype@gmail.com>
2018-04-25 20:47:25 -0300

    Ocurrence list is now showing the data

--- src/components/occurrenceList.js
+++ src/components/occurrenceList.js
@@ -1,32 +1,48 @@
 import React, {Component} from 'react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
+import axios from 'axios';
 
 class OccurrenceList extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            occurrence: []
+        };
+    }
+    componentDidMount(){
+        axios.get('http://localhost:8080/api/ocorrencia').then((response) => {
+            this.setState({occurrence: response.data});
+        })
+    }
+    renderOcurrence(ocurrence){
+        return ocurrence.map(item => {
+            return(
+                <Card className={'card'}>
+                    <CardHeader
+                        title={item.nome_usuario}
+                        subtitle={`${item.cidade} - ${item.estado}`}
+                        avatar="images/jsa-128.jpg"
+                    />
+                    <CardMedia>
+                        <img
+                            src="https://static.independent.co.uk/s3fs-public/styles/story_large/public/thumbnails/image/2013/01/24/12/v2-cute-cat-picture.jpg"
+                            alt=""/>
+                    </CardMedia>
+                    <CardText>{item.descricao}</CardText>
+                    <CardActions>
+                        <FlatButton label="Enviar"/>
+                        <FlatButton label="Deletar"/>
+                    </CardActions>
+                </Card>
+            );
+        });
+    }
     render() {
         return (
-            <Card className={'card'}>
-                <CardHeader
-                    title="URL Avatar"
-                    subtitle="Subtitle"
-                    avatar="images/jsa-128.jpg"
-                />
-                <CardMedia overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle"/>}>
-                    <img
-                        src="https://static.independent.co.uk/s3fs-public/styles/story_large/public/thumbnails/image/2013/01/24/12/v2-cute-cat-picture.jpg"
-                        alt=""/>
-                </CardMedia>
-                <CardText>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-                    Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-                    Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-                </CardText>
-                <CardActions>
-                    <FlatButton label="Enviar"/>
-                    <FlatButton label="Deletar"/>
-                </CardActions>
-            </Card>
+            <div>
+                {this.renderOcurrence(this.state.occurrence)}
+            </div>
         );
     }
 }

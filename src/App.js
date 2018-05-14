bc25db560fe47419a859eb1721331234a4424d27
Igor Phelype Guimarães <igor.phelype@gmail.com>
2018-05-14 01:38:14 -0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
 import Routes from "./routes/routes";
 import theme from './theme.js';
-/*
-    TODO: desenvolver uma forma melhor de gerenciar o localstorage, talvez uma classe ou achar alguma lib
- */
+
 class App extends Component {
     componentDidMount(){
         const occurrences = JSON.parse(localStorage.getItem('occurrences'));
